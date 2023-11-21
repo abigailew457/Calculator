7abde658934bacddd021f3e9dbee978650b52d79
abigailew457 <abigailew457@gmail.com>
2023-11-21 20:49:41 +0900

    Add basic integer functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(number) {
+    return number + number;
+};
+
+function subtract(number) {
+    return number - number;
+};
+
+function multiply(number) {
+    return number * number;
+}; 
+
+function divide(number) {
+    return number/number;
+};
