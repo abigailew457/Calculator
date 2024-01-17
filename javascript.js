082b11bdc8e3fa9ccf299c9dc429eb5c942d9fb4
abigailew457 <abigailew457@gmail.com>
2024-01-17 20:41:08 +0900

    Create operate function

--- javascript.js
+++ javascript.js
@@ -3,6 +3,18 @@ let sub = (a,b) => a - b;
 let multiply = (a,b) => a*b;
 let divide = (a,b) => a/b;
 
-let number = 0;
-let number2 = 0;
-let operator = 0;
+
+function operate(operator, num1, num2) {
+
+    if (operator == add) {
+        return add(num1,num2);
+    } else if (operator == sub) {
+        return sub(num1,num2);
+    } else if (operator == multiply) {
+        return multiply(num1,num2);
+    } else if (operator == divide) {
+        return divide(num1,num2);
+    } else {
+        return(error);
+    };
+}
